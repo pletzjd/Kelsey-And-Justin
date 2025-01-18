46a667925024d1dc5014fda766d2712acde35e2d
pletzjd <j.pletzer17@gmail.com>
2025-01-18 12:57:39 -0500

    Instead of a registry section added a question about registry to FAQ page. Created template for additional questions.

--- kelsey-and-justin/src/components/pages/FAQ.js
+++ kelsey-and-justin/src/components/pages/FAQ.js
@@ -3,7 +3,17 @@ import React from 'react';
 export default function FAQ() {
   return (
     <div>
-      <h1>FAQ</h1>
+      <h3>If you have any questions that are not address here feel free to reach out to us via email or text</h3>
+
+      <div>
+        <h1>DO YOU GUYS HAVE A GIFT REGISTRY?</h1>
+        <h3>If you would like to get us a wedding gift we much prefer cash.</h3>
+      </div>
+
+      <div>
+        <h1>QUESTION?</h1>
+        <h3>Answer.</h3>
+      </div>
     </div>
   );
 }
