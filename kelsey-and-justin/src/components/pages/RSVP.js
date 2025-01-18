53e15c9e11f0c6a0e858fae6a3b45d1d4cb7ca06
pletzjd <j.pletzer17@gmail.com>
2025-01-18 13:08:48 -0500

    set up content for RSVP page.

--- kelsey-and-justin/src/components/pages/RSVP.js
+++ kelsey-and-justin/src/components/pages/RSVP.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function RSVP() {
   return (
     <div>
-      <h1>RSVP</h1>
+      <h3>Please fill out the google form linked below to RSVP. We look forward to having you with us on this magic day!</h3>
+
+      <button>
+        <a href='https://docs.google.com/forms/d/e/1FAIpQLSdZZSj9-vOVF1jV1a5XaP_NZSkc_jsW6pdjcragm1vwlTALUA/viewform?vc=0&c=0&w=1&flr=0'>RSVP</a>
+      </button>
     </div>
   );
 }
