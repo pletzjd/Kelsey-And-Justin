9c6c78ac3a7ad6b7881d19890cf99de48d9543b0
pletzjd <j.pletzer17@gmail.com>
2025-01-18 12:46:50 -0500

    set up content for travel page.

--- kelsey-and-justin/src/components/pages/Travel.js
+++ kelsey-and-justin/src/components/pages/Travel.js
@@ -3,7 +3,27 @@ import React from 'react';
 export default function Travel() {
   return (
     <div>
-      <h1>Travel</h1>
+      <h3>For our guests coming from out of town</h3>
+
+      <div>
+        <img src='' height='' width='' alt='Hotel_Icon'></img>
+        <h3>Hotel</h3>
+        <h1>Hotel Name</h1>
+        <p>We've reserved a block of rooms for our guests to rent with. Use Discount code: PLACEHOLDER. Don't worry if you aren't able to snag one. There are many others in the area as well.</p>
+        <button>
+          <a href=''>View</a>
+        </button>
+      </div>
+
+      <div>
+        <img src='' height='' width='' alt='House_Icon'></img>
+        <h3>Other Accomidations</h3>
+        <h1>AirBNB</h1>
+        <p>Aside from hotels AirBNB is another great options. If you want something close to the venue look for rentals in and around the Centertown area.</p>
+        <button>
+          <a href='https://www.airbnb.ca/s/Centretown--Ottawa--Canada/homes?refinement_paths%5B%5D=%2Fhomes&place_id=ChIJixGuCK4FzkwR2sdKuIM2Xl4&adults=1'>View</a>
+        </button>
+      </div>
     </div>
   );
 }
