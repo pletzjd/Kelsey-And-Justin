c5b4138edd7f984f3994b540e7410670884fdebd
pletzjd <j.pletzer17@gmail.com>
2025-01-18 11:48:51 -0500

    Created basic page content for home page

--- kelsey-and-justin/src/components/pages/Home.js
+++ kelsey-and-justin/src/components/pages/Home.js
@@ -3,7 +3,34 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <img className='banner' src='' height='' width='' alt=''></img>
+      
+      <div>
+        <h1>Justing Wittiveen</h1>
+        <h5>and</h5>
+        <h1>Kelsey Pletzer</h1>
+      </div>
+      
+      <div>
+        <h2>April 26, 2025</h2>
+        <h4>Ottawa, ON</h4>
+      </div>
+      
+      <button>
+        <a href='https://docs.google.com/forms/d/e/1FAIpQLSdZZSj9-vOVF1jV1a5XaP_NZSkc_jsW6pdjcragm1vwlTALUA/viewform?vc=0&c=0&w=1&flr=0'>RSVP</a>
+      </button>
+
+      <div>
+        <h2>How We Meet</h2>
+        <h4>Placeholder Date</h4>
+        <p>Coming Soon!</p>
+      </div>
+
+      <div>
+        <h2>The Proposal</h2>
+        <h4>Placeholder Date</h4>
+        <p>Coming Soon!</p>
+      </div>
     </div>
   );
 }
