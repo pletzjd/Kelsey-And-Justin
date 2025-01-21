567bf5b4597a84b609fc9810b138ba642d122621
pletzjd <j.pletzer17@gmail.com>
2025-01-20 20:55:54 -0500

    added basic formating for FAQ and RSVP pages

--- kelsey-and-justin/src/components/pages/FAQ.js
+++ kelsey-and-justin/src/components/pages/FAQ.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function FAQ() {
   return (
-    <div>
-      <h3>If you have any questions that are not address here feel free to reach out to us via email or text</h3>
+    <div className='main-content'>
+      <h3 className='opener'>If you have any questions that are not address here feel free to reach out to us via email or text</h3>
 
-      <div>
+      <div className='card'>
         <h1>DO YOU GUYS HAVE A GIFT REGISTRY?</h1>
         <h3>If you would like to get us a wedding gift we much prefer cash.</h3>
       </div>
 
-      <div>
+      <div className='card'>
         <h1>QUESTION?</h1>
         <h3>Answer.</h3>
       </div>
--- kelsey-and-justin/src/components/pages/RSVP.js
+++ kelsey-and-justin/src/components/pages/RSVP.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 export default function RSVP() {
   return (
-    <div>
-      <h3>Please fill out the google form linked below to RSVP. We look forward to having you with us on this magic day!</h3>
+    <div className='main-content'>
+      <h3 className='opener'>Please fill out the google form linked below to RSVP. We look forward to having you with us on this magic day!</h3>
 
-      <button>
-        <a href='https://docs.google.com/forms/d/e/1FAIpQLSdZZSj9-vOVF1jV1a5XaP_NZSkc_jsW6pdjcragm1vwlTALUA/viewform?vc=0&c=0&w=1&flr=0'>RSVP</a>
-      </button>
+        <a className='button' href='https://docs.google.com/forms/d/e/1FAIpQLSdZZSj9-vOVF1jV1a5XaP_NZSkc_jsW6pdjcragm1vwlTALUA/viewform?vc=0&c=0&w=1&flr=0'>RSVP</a>
     </div>
   );
 }
