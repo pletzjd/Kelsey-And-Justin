3ecd651de4da7f0796b97ca913e82219e6e4ce1b
pletzjd <j.pletzer17@gmail.com>
2025-01-28 13:52:28 -0500

    added a font for less fancy text.

--- kelsey-and-justin/src/components/pages/Home.js
+++ kelsey-and-justin/src/components/pages/Home.js
@@ -20,13 +20,13 @@ export default function Home() {
       <a className='button' href='https://docs.google.com/forms/d/e/1FAIpQLSdZZSj9-vOVF1jV1a5XaP_NZSkc_jsW6pdjcragm1vwlTALUA/viewform?vc=0&c=0&w=1&flr=0'>RSVP</a>
       
       <div className='card'>
-        <h2>How We Meet</h2>
+        <h2 className='fancy fancy-names'>How We Meet</h2>
         <h4>Placeholder Date</h4>
         <p>Coming Soon!</p>
       </div>
 
       <div className='card'>
-        <h2>The Proposal</h2>
+        <h2 className='fancy fancy-names'>The Proposal</h2>
         <h4>Placeholder Date</h4>
         <p>Coming Soon!</p>
       </div>
