cee2828b7746e81986a8a10eb26edc72c6a8060f
pletzjd <j.pletzer17@gmail.com>
2025-02-03 15:30:20 -0500

    Added a modal that opens when an image in the gallery is clicked with arrows buttons to switch between the next and previous image.

--- kelsey-and-justin/src/components/pages/Gallery.js
+++ kelsey-and-justin/src/components/pages/Gallery.js
@@ -1,15 +1,66 @@
 // Note: Image data importing and object can be found in App.js
 import React, { useEffect, useState } from 'react';
+import Modal from "../Modal";
 
 export default function Gallery({imgData}) {
+  const [clickedImg, setClickedImg] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(null);
+
+  const handleClick = (item, index) => {
+    setCurrentIndex(index);
+    setClickedImg(item.imgSmall);
+  };
+
+  const handelRotationRight = () => {
+  const totalLength = imgData.length;
+  if (currentIndex + 1 >= totalLength) {
+    setCurrentIndex(0);
+    const newUrl = imgData[0].imgSmall;
+    setClickedImg(newUrl);
+    return;
+  }
+  const newIndex = currentIndex + 1;
+  const newUrl = imgData.filter((item) => {
+    return imgData.indexOf(item) === newIndex;
+  });
+  const newItem = newUrl[0].imgSmall;
+  setClickedImg(newItem);
+  setCurrentIndex(newIndex);
+};
+
+const handelRotationLeft = () => {
+  const totalLength = imgData.length;
+  if (currentIndex === 0) {
+    setCurrentIndex(totalLength - 1);
+    const newUrl = imgData[totalLength - 1].imgSmall;
+    setClickedImg(newUrl);
+    return;
+  }
+  const newIndex = currentIndex - 1;
+  const newUrl = imgData.filter((item) => {
+    return imgData.indexOf(item) === newIndex;
+  });
+  const newItem = newUrl[0].imgSmall;
+  setClickedImg(newItem);
+  setCurrentIndex(newIndex);
+};
 
   return (
     <div className='gallery-content'>
       <div className='gallery-container'>
         {imgData.map((item, index) => (
-          <img src={item.imgSmall} alt={item.text} className='gal-image' key={index}></img>
+          <img src={item.imgSmall} alt={item.text} className='gal-image' key={index} onClick={() => handleClick(item, index)}></img>
         ))}
       </div>
+
+      {clickedImg && (
+          <Modal
+            clickedImg={clickedImg}
+            handelRotationRight={handelRotationRight}
+            setClickedImg={setClickedImg}
+            handelRotationLeft={handelRotationLeft}
+          />
+        )}
     </div>
   );
 }
