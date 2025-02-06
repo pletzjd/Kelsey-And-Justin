cee70b86b514b4fd8221a3a1310e5754aac06c72
pletzjd <j.pletzer17@gmail.com>
2025-02-06 16:49:07 -0500

    Made the site mobile friendly

--- kelsey-and-justin/src/components/pages/RSVP.js
+++ kelsey-and-justin/src/components/pages/RSVP.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function RSVP({imgData}) {
   return (
     <div className='main-content'>
-      <h3 className='opener rsvp-opener'>Please fill out the google form linked below to RSVP. We look forward to having you with us on this magic day!</h3>
+      <h3 className='opener rsvp-opener fancy fancy-symbols'>Please fill out the google form linked below to RSVP. We look forward to having you with us on this magic day!</h3>
 
         <a className='button' href='https://docs.google.com/forms/d/e/1FAIpQLSdZZSj9-vOVF1jV1a5XaP_NZSkc_jsW6pdjcragm1vwlTALUA/viewform?vc=0&c=0&w=1&flr=0'>RSVP</a>
 
