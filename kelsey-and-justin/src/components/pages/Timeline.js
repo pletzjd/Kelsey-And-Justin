5eb32bf2cc44dda2f2aa62c6eea0d260ced5e0fc
pletzjd <j.pletzer17@gmail.com>
2025-04-16 11:35:03 -0400

    changed from using two different images at different screen sizes to one image over top wrapper div with a background matching the image background using only the slim image.

--- kelsey-and-justin/src/components/pages/Timeline.js
+++ kelsey-and-justin/src/components/pages/Timeline.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import timelineLrg from '../../images/timeline-lrg.jpg';
-import timelineSml from '../../images/timeline-sml.jpg';
+import timeline from '../../images/timeline.jpg';
 
 export default function Timeline() {
   return (
     <div className='main-content'>
       <h3 className='opener fancy fancy-symbols'>Here's what to expect for our wedding day. We can't wait to celebrate with you!</h3>
 
-      <img
-      src={timelineSml}
-      srcSet={timelineSml + ' 900w, ' + timelineLrg + ' 1100w'}
-      className='timeline'>
-      </img>
+      <div className='timelineWrapper'>
+        <img src={timeline} className='timeline'></img>
+      </div>
+      
     </div>
   );
 }
